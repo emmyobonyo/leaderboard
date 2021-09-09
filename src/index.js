5db98b9cdbd7faec86d9bb2b15d987e84b696b7c
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-09-09 23:14:37 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ const refreshButton = document.getElementById('refresh');
 const ul = document.getElementById('ul');
 
 refreshButton.addEventListener('click', () => {
+  ul.innerHTML = '';
   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Gl6jBTKnXWBvnQjOirPR/scores/')
-  .then((res) => res.json())
-  .then((data) => {
-    const user = data.result;
-    user.forEach((user) => {
-      const list = document.createElement('li');
-      list.innerHTML = `${user.user} : ${user.score}`;
-      ul.appendChild(list);
-    })
-  });
-
-})
+    .then((res) => res.json())
+    .then((data) => {
+      const user = data.result;
+      user.forEach((user) => {
+        const list = document.createElement('li');
+        list.innerHTML = `${user.user} : ${user.score}`;
+        ul.appendChild(list);
+      });
+    });
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -29,16 +29,15 @@ form.addEventListener('submit', (e) => {
   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Gl6jBTKnXWBvnQjOirPR/scores', {
     method: 'POST',
     headers: {
-      'Accept': 'application/json, text/plain, "/"',
-      'Content-type': 'application/json'
+      Accept: 'application/json, text/plain, "/"',
+      'Content-type': 'application/json',
     },
-    body: JSON.stringify({user:name, score:score})
+    body: JSON.stringify({ user: name, score }),
   })
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+    .then((res) => res.json());
   inputName.value = '';
   inputScore.value = '';
-})
+});
 
 function component() {
   const element = document.createElement('div');
