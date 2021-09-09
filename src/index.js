7d305186e09384da44098d3e6c824212cc45266f
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-09-09 20:39:58 +0300

    Add Post method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 // import _ from 'lodash';
 import './style.css';
 
+const form = document.getElementById('form');
+const inputName = document.getElementById('name');
+const inputScore = document.getElementById('score');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = inputName.value;
+  const score = inputScore.value;
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Gl6jBTKnXWBvnQjOirPR/scores', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, "/"',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({user:name, score:score})
+  })
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+})
+
 function component() {
   const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
